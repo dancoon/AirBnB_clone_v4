84f9b997b316d8241460088ac79a46ba16be1f07
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-08-17 14:18:42 +0300

    Scripts

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,29 +1,29 @@
-$(document).ready(init);
+$(document).ready(() => init);
 
 const HOST = window.location.hostname;
 
-function init () {
+function init() {
   const amenityObj = {};
-  $('.amenities .popover input').change(function () {
-    if ($(this).is(':checked')) {
-      amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
-    } else if ($(this).is(':not(:checked)')) {
-      delete amenityObj[$(this).attr('data-name')];
+  $(".amenities .popover input").change(function () {
+    if ($(this).is(":checked")) {
+      amenityObj[$(this).attr("data-name")] = $(this).attr("data-id");
+    } else if ($(this).is(":not(:checked)")) {
+      delete amenityObj[$(this).attr("data-name")];
     }
     const names = Object.keys(amenityObj);
-    $('.amenities h4').text(names.sort().join(', '));
+    $(".amenities h4").text(names.sort().join(", "));
   });
 
   apiStatus();
 }
 
-function apiStatus () {
+function apiStatus() {
   const API_URL = `http://${HOST}:/api/v1/status/`;
   $.get(API_URL, (data, textStatus) => {
-    if (textStatus === 'success' && data.status === 'OK') {
-      $('#api_status').addClass('available');
+    if (textStatus === "success" && data.status === "OK") {
+      $("#api_status").addClass("available");
     } else {
-      $('#api_status').removeClass('available');
+      $("#api_status").removeClass("available");
     }
   });
 }
